84f9c38b88a5627772ba1ce93e76f3688b317643
zhenzhen <1161343044@qq.com>
2020-06-21 19:07:18 +0800

    0621
    Signed-off-by: zhenzhen <1161343044@qq.com>

--- website/gulpfile.js
+++ website/gulpfile.js
@@ -1,14 +1,13 @@
 const gulp = require("gulp");
 var sass = require('gulp-sass'); 
 var connect = require('gulp-connect');
-gulp.task("copyIndex", done => {
-    gulp.src("html/homePage.html").pipe(gulp.dest("dist"));
-    console.log(123);
-    done();
-})
+
 gulp.task('watch',done=>{
-    gulp.watch('html/copyIndex.html', gulp.series("copyIndex")); 
-    // gulp.watch('img/', gulp.series("copyImg"));
+    gulp.watch('html/**', gulp.series("copyIndex")); 
+    gulp.watch('img/**', gulp.series("copyImg")); 
+    gulp.watch('js/**', gulp.series("copyJs")); 
+    gulp.watch('css/**', gulp.series("copyCss"));  
+    // gulp.watch('img/', gulp.series("copyImg")); 
     done()
  }) 
  gulp.task("server", done => {
@@ -19,6 +18,28 @@ gulp.task('watch',done=>{
     done();
 });
  //!json/b.json排除某个文件 
+ gulp.task("default", gulp.series("watch", "server"));
+ gulp.task('scripts',done=>{ 
+      gulp.src(['js/jquery.js','js/homePage.js'])
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('dist/js'));
+       done();
+ })
 
 
-
+gulp.task("copyImg",done=>{
+    gulp.src("img/**").pipe(gulp.dest("dist/img"));
+    done();
+})
+gulp.task("copyIndex", done => {
+    gulp.src("html/**").pipe(gulp.dest("dist/html"));
+    done();
+})
+gulp.task("copyJs", done => {
+    gulp.src("js/**").pipe(gulp.dest("dist/js"));
+    done();
+})
+gulp.task("copyCss", done => {
+    gulp.src("css/**").pipe(gulp.dest("dist/css"));
+    done();
+})
